4643ef10064f1e3874a9fd0f8afff949b3dc5c89
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-04-19 15:55:16 +0200

    feat: create validate email

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
 import { AuthService } from "../services/auth.service";
+import { EmailService } from "../services/email.service";
+import { envs } from "../../config/envs";
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const controller = new AuthController(new AuthService());
+    const emailService = new EmailService(
+      envs.MAILER_SERVICE,
+      envs.MAILER_EMAIL,
+      envs.MAILER_SECRET_KEY
+    );
+    const controller = new AuthController(new AuthService(emailService));
 
     router.post("/login", controller.loginUser);
     router.post("/register", controller.registerUser);
--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,14 +1,16 @@
 import { bcrypAdapter } from "../../config/bcrypt.adapter";
+import { envs } from "../../config/envs";
 import { JwtAdapter } from "../../config/jwt.adapter";
 import { UserModel } from "../../data/mongo/models/user.models";
 import { LoginUserDto } from "../../domain/dtos/auth/login-user.dto";
 import { RegisterUserDto } from "../../domain/dtos/auth/register-user.dto";
 import { UserEntity } from "../../domain/entities/user.entity";
 import { CustomError } from "../../domain/errors/custom.errors";
+import { EmailService } from "./email.service";
 
 export class AuthService {
   // DI
-  constructor() {}
+  constructor(private readonly emailService: EmailService) {}
 
   public async registerUser(registerUSerDto: RegisterUserDto) {
     const existUser = await UserModel.findOne({ email: registerUSerDto.email });
@@ -25,7 +27,7 @@ export class AuthService {
 
       if (!token) throw CustomError.badRequest("Error while creating JWT");
 
-      // email de confirmacion
+      await this.sendEmailValidationLink(user.email);
 
       const { password, ...userEntity } = UserEntity.fromObject(user);
 
@@ -55,4 +57,43 @@ export class AuthService {
 
     return { user: userEntity, token };
   }
+
+  private sendEmailValidationLink = async (email: string) => {
+    const token = await JwtAdapter.generateToken({ email });
+    if (!token) throw CustomError.internalServer("Error getting token");
+
+    const link = `${envs.WEBSERVICE_URL}/auth/validate-email/${token}`;
+    const html = `
+    <h1>Validate your email</h1>
+    <p>Click on the following link to validate your email</p>
+    <a href="${link}">Validate your email:${email}</a>
+    `;
+
+    const options = {
+      to: email,
+      subject: "Validate your email",
+      htmlBody: html,
+    };
+
+    const isSet = await this.emailService.sendEmail(options);
+    if (!isSet) throw CustomError.internalServer("Error sending email");
+
+    return true;
+  };
+
+  public validateEmail = async (token: string) => {
+    const payload = await JwtAdapter.validateToken(token);
+    if (!payload) throw CustomError.badRequest("Invalid token");
+
+    const { email } = payload as { email: string };
+    if (!email) throw CustomError.internalServer("Email not in token");
+
+    const user = await UserModel.findOne({ email });
+    if (!user) throw CustomError.badRequest("Email not exist");
+
+    user.emailValidated = true;
+    await user.save();
+
+    return true;
+  };
 }
